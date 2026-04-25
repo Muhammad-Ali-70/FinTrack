7f0dcad781df2ff729959acf4c19a9536494f65d
Muhammad Ali <alihere786@gmail.com>
2026-04-26 02:45:58 +0500

    Basic Folder Setup and Router Files

---
+++ app/(onboarding)/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Index = () => {
+    return (
+        <View>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+            <Text>index</Text>
+
+        </View>
+    )
+}
+
+export default Index
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const [isOnboarded, setIsOnboarded] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const check = async () => {
+      const value = await AsyncStorage.getItem("isOnboarded");
+      setIsOnboarded(value === "true");
+    };
+
+    check();
+  }, []);
+
+  if (isOnboarded === null) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  if (!isOnboarded) {
+    return <Redirect href="/(onboarding)" />;
+  }
+
+  return <Redirect href="/(tabs)" />;
 }
